0d411b7d6c6060594c27152077b93070fc319063
Angel Zhang <anzhouzhang913@gmail.com>
2024-09-01 23:10:17 -0400

    Set up slash commands

--- bach-bot.py
+++ bach-bot.py
@@ -1,117 +1,52 @@
 import os
 import discord
-import youtube_dl
-from youtube_search import YoutubeSearch
-
+from discord import app_commands
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-YOUTUBE_WATCH = "https://www.youtube.com/watch?v="
 
-queue = []
-vc = None
-is_playing = False
-
-# set intents
+# Set intents
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
 
-# initialize
-client = discord.Client(intents=intents)
+# Create the bot
+bot = commands.Bot(command_prefix="/", intents=intents)
 
-def play_next(e):
-    global queue, vc
-    queue.pop()
-    if len(queue) > 0:
-        url = f"{YOUTUBE_WATCH}{queue[0]['id']}"
-        vc.play(discord.FFmpegPCMAudio(url))
-
-def download(url, id):
-    opts = {'format': 'bestaudio/best',
-                'postprocessors': [{
-                    'key': 'FFmpegExtractAudio',
-                    'preferredcodec': 'mp3',
-                    'preferredquality': '192'
-                }],
-                'outtmpl': id}
-    with youtube_dl.YoutubeDL(opts) as ydl:
-        ydl.download([url])
-
-# when the bot is ready
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Logged in as {client.user.name}')
-    print(client.user.id)
+    await bot.tree.sync()
+    print(f'Logged in as {bot.user}')
 
-# when the bot gets a message
-@client.event
-async def on_message(message):
-    global queue, vc, is_playing
+@bot.tree.command(name="play", description="Play music from a link")
+async def play(interaction: discord.Interaction, link: str):
+    await interaction.response.send_message(f"Your link was {link}")
 
-    if message.author == client.user:
-        return
+@bot.tree.command(name="composer", description="Play music by a specific composer")
+async def composer(interaction: discord.Interaction, composer: str):
+    await interaction.response.send_message(f"Your specified era was {composer}")
 
-    tokens = message.content.split(' ')
-    print(tokens)
+era_choices = [
+    app_commands.Choice(name="Renaissance", value="renaissance"),
+    app_commands.Choice(name="Baroque", value="baroque"),
+    app_commands.Choice(name="Classical", value="classical"),
+    app_commands.Choice(name="Romantic", value="romantic"),
+    app_commands.Choice(name="20th Century", value="20th_century")
+]
 
-    # ignore other messages
-    if tokens[0] != 'bwv' and tokens[0] != 'js':
-        return
+@bot.tree.command(name="era", description="Play music from a specific era")
+async def era(interaction: discord.Interaction, choice: str):
+    await interaction.response.send_message(f"Your specified era was {choice}")
 
-    print(message.author.voice)
+@era.autocomplete("choice")
+async def autocomplete(interaction: discord.Interaction, current: str):
+    return [
+        app_commands.Choice(name=choice.name, value=choice.value)
+        for choice in era_choices if current.lower() in choice.name.lower()
+    ]
 
-    if tokens[1].isnumeric():
-        # join the voice channel
-        if message.author.voice:
-            try:
-                channel = message.author.voice.channel
-                vc = await channel.connect()
-            except:
-                print("Already in voice channel")
-        else:
-            await message.channel.send("❌ Not connected to a voice channel")
-        # search for music
-        results = YoutubeSearch(f'bwv {tokens[1]}', max_results=10).to_dict()
-        print(results[0])
-        # play or queue
-        queue.append(results[0])
-        if is_playing:
-            await message.channel.send(f"✅ Added '{results[0]['title']}' to the queue")
-        else:
-            url = f"{YOUTUBE_WATCH}{queue[0]['id']}"
-            # download(url, queue[0]['id'])
-            vc.play(discord.FFmpegPCMAudio(source="test.mp3",
-                                           executable='../../ffmpeg/bin/ffmpeg.exe'),
-                    after=play_next)
-            await message.channel.send(f"▶️ Playing {url}")
-            is_playing = True
-
-    elif tokens[1] == 'pause':
-        if vc and vc.is_playing():
-            vc.pause()
-            await message.channel.send("⏸️ Paused")
-        else:
-            await message.channel.send("❌ Not playing anything")
-
-    elif tokens[1] == 'resume':
-        if vc and vc.is_paused():
-            vc.resume()
-            await message.channel.send("⏯️ Resumed")
-
-    elif tokens[1] == 'skip':
-        if vc and vc.is_playing:
-            vc.stop()
-            queue.pop()
-        else:
-            await message.channel.send("❌ Not playing anything")
-
-    elif tokens[1] == 'stop':
-        if vc:
-            await message.channel.disconnect()
-            queue = []
-
-# start the bot
-client.run(BOT_TOKEN)
+# Start the bot
+bot.run(BOT_TOKEN)
